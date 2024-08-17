076f13f952d032108a79558c45764f5afd9cef65
Akshat Khurana <akshat.khurana@yahoo.com>
2024-08-18 09:45:11 +1000

    Half-working slider volume system
    After a very frustrating hour - I finally managed to get the sliding input working, but the LED bar still barely lights up

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from machine import Pin, I2C, ADC
+from machine import Pin, I2C, ADC, PWM
 import ssd1306
 import time
 
@@ -6,17 +6,43 @@ import time
 WIDTH = 128
 HEIGHT = 64
 
+vol = 0
 # Assign all the pins
-pad_row_pins = []
-pad_col_pins = []
-
-slider_pin = Pin(32, Pin.IN)
 screen_pin = I2C(0, 
              scl=Pin(22), 
              sda=Pin(21))
+screen = ssd1306.SSD1306_I2C(WIDTH, HEIGHT, screen_pin)
 
-buzzer_pin = Pin(12,Pin.OUT)
+slider_pin = Pin(34)
+slider = ADC(slider_pin)
+slider.width(ADC.WIDTH_12BIT)
+slider.atten(ADC.ATTN_11DB)
+
+LED_bar_pins = [13, 12, 14, 27, 26, 25, 33, 32, 35, 19]
+
+pad_row_pins = []
+pad_col_pins = []
+
+buzzer_pin = Pin(23)
 buzzer_pwm = PWM(buzzer_pin)
+
+# Functions
+def play_note(note, duration):
+  buzzer_pwm.freq(notes[note])
+  buzzer_pwm.duty(512)
+  time.sleep_ms(duration)
+  buzzer_pwm.duty(0)
+
+def show_bar_LED(count):
+    for pin in LED_bar_pins[:count]:
+        led = Pin(pin, Pin.OUT)
+        led.on()
+
+def clear_screen():
+  screen.fill(0)
+
+def convert_reading(reading):
+  return int(reading * 9 / 4095)
 
 # Buzzer stuff
 notes = {
@@ -30,18 +56,11 @@ notes = {
     'C5': 523.25,
 }
 
-def play(note, duration):
-  buzzer_pwm.freq(notes[note])
-  buzzer_pwm.duty(512)
-  time.sleep_ms(duration)
-  buzzer_pwm.duty(0)
-
 # Main code
-screen = ssd1306.SSD1306_I2C(WIDTH, HEIGHT, screen_pin)
-play('C4', 100)
-
-screen.text('Dashboard', 10, 2)      
-screen.show()
+show_bar_LED(10)
+while True:
+  vol = convert_reading(slider.read())
+  show_bar_LED(vol)
 
 # Closing code
 buzzer_pwm.deinit()
